c08325a3db29431c9aea75e1fdb37b2340421619
Abish <ava.caughdough@outlook.com>
2022-12-04 21:09:06 -0500

    Added day 4

---
+++ Day 4/part-2.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(require threading)
+
+(define (file-to-lines file-name)
+  (~> (port->string (open-input-file file-name) #:close? #t)
+      (string-split _ "\n")
+      (map (λ (line) (string-trim line)) _)))
+
+(define (lines-to-elves lst)
+  (map (λ (line) (string-split line ",")) lst))
+
+(define (string-to-range str)
+  (let ((boundaries (~> str
+                        (string-split _ "-")
+                        (map string->number _))))
+    (range (car boundaries) (+ (cadr boundaries) 1))))
+
+(define (elves-to-assignments lst)
+  (map (λ (group) (let ((elf-a (car group))
+                        (elf-b (cadr group)))
+                    (list (string-to-range elf-a)
+                          (string-to-range elf-b))))
+       lst))
+
+(define (determine-overlap lst)
+  (map (λ (group) (cond
+                    ((not (empty? (set-intersect (car group) (cadr group)))) #f)
+                    (else #t)))
+       lst))
+
+(define (solution file-name)
+  (~> file-name
+      (file-to-lines)
+      (lines-to-elves)
+      (elves-to-assignments)
+      (determine-overlap)
+      (filter false? _)
+      (length)))
+
+(solution "./puzzle-input.dat")
